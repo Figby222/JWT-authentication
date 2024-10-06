05fee8d0365641fc19eef177c853fbc9900c3ced
Figby222 <figby222@outlook.com>
2024-10-06 11:05:23 -0700

    Add /api/login POST route

--- routers/indexRouter.mjs
+++ routers/indexRouter.mjs
@@ -9,4 +9,6 @@ indexRouter.get("/api", indexController.APIRouteGet);
 
 indexRouter.post("/api/posts", indexController.createPostPost);
 
+indexRouter.post("/api/login", indexController.loginPost);
+
 export default indexRouter;
